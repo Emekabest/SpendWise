cfff98d18c51965ba4d59039f4cf2bc864784081
EmekaBest <josephemeka2611@gmail.com>
2025-09-24 02:59:12 +0100

    made changes

--- app/homescreen.tsx
+++ app/homescreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Dimensions, SafeAreaView, Text, View } from "react-native";
+import { Dimensions, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import AppDetails from "./service/AppService";
 
 
@@ -15,21 +15,25 @@ const HomeScreen  = ()=>{
         <SafeAreaView className="flex-1 p-4" style={{height:height, backgroundColor:"#fff"}}>
             <View className="h-[10%] flex-row">{/**Header Seciton */}
                 <View className="h-[100%] w-[50%] justify-center">
-                    <Text className="text-lg font-bold text-gray-800">Hi, {userName}</Text>
+                    <Text className="text-lg font-monasans-bold text-gray-800">Hi, {userName}</Text>
                 </View>
                 <View></View>
             </View>
-            <View className="h-[12%] w-[100%] rounded-2xl flex-row p-2 justify-between" style={{backgroundColor:AppDetails.color.iconColors}}>{/**Balance and Funds Section */}
-                <View className="h-[100%] w-[48%]">
-                    <View><Text>Balance</Text></View>
-                    <View><Text>10000</Text></View>
-
+            <View className="h-[15%] w-[100%] rounded-2xl flex-row p-4 justify-between" style={{backgroundColor:AppDetails.color.iconColors}}>{/**Balance and Funds Section */}
+                <View className="h-full w-[48%] justify-between">
+                    <Text className="text-white/80 font-monasans-light text-base">Balance</Text>
+                    <Text className="text-white text-2xl font-monasans-bold">₦10,000</Text>
                 </View>
 
 
-                <View className="h-[100%] w-[48%]">
-
+                <View className="h-full w-[48%] items-end justify-between">
+                    <TouchableOpacity><Text className="text-white/80 font-monasans-light text-sm">Transaction history</Text></TouchableOpacity>
+                    <TouchableOpacity activeOpacity={1} className="bg-white py-2 px-4 rounded-full"><Text className="font-monasans-bold" style={{color:AppDetails.color.iconColors}}>Add Funds</Text></TouchableOpacity>
                 </View>
+            </View>
+
+            <View>{/**Categories Section */}
+                
             </View>
 
         </SafeAreaView>
