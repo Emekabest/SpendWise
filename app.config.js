bf311b8b3ab8695dd29566878b6d1d6695ee1a37
EmekaBest <josephemeka2611@gmail.com>
2025-10-13 00:30:00 +0100

    changed the app.config

--- app.config.js
+++ app.config.js
@@ -1,5 +1,9 @@
 export default ({ config }) => ({
   ...config,
+  android: {
+    ...config.android,
+    package: "com.jojo104.spendwise", // ✅ add your Android package name here
+  },
   extra: {
     ...config.expo?.extra, // Preserve existing properties
     API_URL: process.env.API_URL,
