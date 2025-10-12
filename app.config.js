e1a17659fc4d75acca886c0bd50ef9c3d2aa4c37
EmekaBest <josephemeka2611@gmail.com>
2025-10-12 23:59:24 +0100

    changed the app.config

--- app.config.js
+++ app.config.js
@@ -1,6 +1,13 @@
 export default ({ config }) => ({
   ...config,
+  expo: {
+    ...config.expo,
   extra: {
+    ...config.expo?.extra, // Preserve existing properties
     API_URL: process.env.API_URL,
+    eas: {
+        projectId: "23723df3-76a2-4333-b942-bbd2003e47b5", // Ensure projectId is included
+    },
   },
+}
 });
