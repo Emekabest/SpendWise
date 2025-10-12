dc32cf85a1112d1b9ce6e05c3b4ab86fe3036a93
EmekaBest <josephemeka2611@gmail.com>
2025-10-13 00:27:16 +0100

    changed the app.config

--- app.config.js
+++ app.config.js
@@ -1,11 +1,7 @@
 export default ({ config }) => ({
   ...config,
   expo: {
-    "name": "SpendWise",
-    "slug": "SpendWise",
-    android: {
-      package: "com.jojo104.spendwise", // 👈 change this to your preferred unique ID
-    },  
+    ...config.expo,
   extra: {
     ...config.expo?.extra, // Preserve existing properties
     API_URL: process.env.API_URL,
